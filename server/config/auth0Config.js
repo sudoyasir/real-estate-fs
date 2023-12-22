b73cbcd541fdff87cf308d890a158f7a821a6195
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-12-22 13:56:32 +0500

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -3,6 +3,6 @@ import { auth } from "express-oauth2-jwt-bearer";
 const jwtCheck = auth({
   audience: "http://localhost:8080",
   issuerBaseURL: "https://dev-k18ywujcrbpggmdi.us.auth0.com",
-//   tokenSigningAlg: "RS256",
+  tokenSigningAlg: "RS256",
 })
 export default jwtCheck;
